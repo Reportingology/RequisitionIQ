c2b87c0f7a892f472143859e585b9cfa1d73e21d
Reportingology <victor@reportingology.com>
2025-10-03 10:31:07 -0400

    Update filled_requisitions.py

--- filled_requisitions.py
+++ filled_requisitions.py
@@ -6,5 +6,5 @@ import datetime
 url_filled_requisitions = "https://raw.githubusercontent.com/Reportingology/RequisitionIQ/refs/heads/main/requisitions/filled_requisitions.csv"
 filled_requisitions = pd.read_csv(url_filled_requisitions)
 
-def print_filled_requisitions(filled_requisitions):
+def print_filled_requisitions():
   print(filled_requisitions)
