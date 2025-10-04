5ceaa3e5a928ea1157278a114e1c8e49de7ba7db
Reportingology <victor@reportingology.com>
2025-10-04 09:50:15 -0400

    Update etl.py

--- etl.py
+++ etl.py
@@ -4,20 +4,30 @@ import datetime
 
 import filled_requisitions
 
-requisition_date_columns = [
+################################################## CONSTANTS ##################################################
+###
+###
+
+date_columns = [
   "requisition_opened_date",   ### Date the requisition was opened in the system
   "date_first_screen",         ### Date the first screen occured on the requisition
   "requisition_filled_date"    ### Date requisition was filled. All reqs are singleheded for ReqIQ MVP
 ]
 
-def format_date_columns(data_frame, requisition_date_columns):
+################################################## FUNCTIONS ##################################################
+###
+###
+
+def format_date_columns(data_frame, date_columns):
   for column in date_columns:
     data_frame[column] = pd.to_datetime(data_frame[column])
     data_frame[f"{column}_month"] = data_frame[column].dt.month
     data_frame[f"{column}_year"] = data_frame[column].dt.year
 
-  return
-  data_frame
+  return data_frame
 
-#### FORMAT DATES #####
-format_date_columns(filled_requisitions, date_columns)
+################################################## ETL PROCESS ##################################################
+###
+###
+
+format_date_columns(filled_requisitions.filled_requisitions, date_columns)
